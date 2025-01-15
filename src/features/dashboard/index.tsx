abfb52c1568d70c8d2c132e258a59cbb5be2d671
fanitepay <131031529+isaacfanite@users.noreply.github.com>
2025-01-15 18:56:16 +0300

    1

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -45,15 +45,6 @@ export default function Dashboard() {
           <div className='w-full overflow-x-auto pb-2'>
             <TabsList>
               <TabsTrigger value='overview'>Overview</TabsTrigger>
-              <TabsTrigger value='analytics' disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value='reports' disabled>
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value='notifications' disabled>
-                Notifications
-              </TabsTrigger>
             </TabsList>
           </div>
           <TabsContent value='overview' className='space-y-4'>
