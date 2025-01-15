7e685f2338828d9e6c8a1a7adee86900ff40784d
fanitepay <131031529+isaacfanite@users.noreply.github.com>
2025-01-15 20:24:22 +0300

    2

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -56,7 +56,7 @@ export const sidebarData: SidebarData = {
         },
         {
           title: 'Transactions',
-          url: '/tasks',
+          url: '/xns',
           icon: IconChecklist,
         },
         {
